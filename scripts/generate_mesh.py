1915c9f90c9685dc9eca664084df6b49e80a2092
Alexandra Graß <alexandra.grass@tum.de>
2019-03-27 16:43:12 +0100

    trying to fix terrain normals

--- scripts/generate_mesh.py
+++ scripts/generate_mesh.py
@@ -47,7 +47,7 @@ normal_floats = [0,0,1,0,0,1,0,0,1,0,0,1,0,1,0,
     0,0,-1,0,0,-1,0,0,-1]
 '''
 vert_floats = coords.flatten()
-normal_floats = [0,0,0]
+normal_floats = np.stack((np.array([0,0,1]),)*cols*rows, axis=0).flatten()
 vert_src = source.FloatSource("cubeverts-array", np.array(vert_floats), ('X', 'Y', 'Z'))
 normal_src = source.FloatSource("cubenormals-array", np.array(normal_floats), ('X', 'Y', 'Z'))
 
@@ -61,7 +61,7 @@ input_list.addInput(1, 'NORMAL', "#cubenormals-array")
 
 # create index array
 '''
-indices = np.array([0,0,2,0,3,0,0,0,3,2,1,3,0,4,1,5,5,6,0,
+indices = numpy.array([0,0,2,1,3,2,0,0,3,2,1,3,0,4,1,5,5,6,0,
     4,5,6,4,7,6,8,7,9,3,10,6,8,3,10,2,11,0,12,
     4,13,6,14,0,12,6,14,2,15,3,16,7,17,5,18,3,
     16,5,18,1,19,5,20,7,21,6,22,5,20,6,22,4,23])
@@ -76,7 +76,7 @@ for x in range(cols-1):
         indices.append([id(x, y), id(x+1, y), id(x, y+1)])
         indices.append([id(x+1, y+1), id(x, y+1), id(x+1, y)])
 indices = np.array(indices).flatten()
-indices = np.stack((indices,np.zeros_like(indices)), axis=1).flatten()
+indices = np.stack((indices,indices), axis=1).flatten()
 
 # create triangle set, add it to list of geometries in the mesh
 triset = geom.createTriangleSet(indices, input_list, "materialref")
