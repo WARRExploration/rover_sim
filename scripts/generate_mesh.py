dc5e570586b37cb6f37e4569e3d024c3062a103e
Alexandra Graß <alexandra.grass@tum.de>
2019-03-28 17:50:38 +0100

    added texture to terrain model

--- scripts/generate_mesh.py
+++ scripts/generate_mesh.py
@@ -34,12 +34,38 @@ ys = np.stack((ys,)*cols, axis=0)
 # coordinates
 coords = np.stack((xs, ys, data), axis=2)
 
-# add a material
+# create the mesh
 mesh = Collada()
+
+# add a material
+'''
 effect = material.Effect("effect0", [], "phong", emission=(0.2,0,0,1.0), diffuse=(0,1.0,0,1.0), specular=(1.0,1.0,1.0,1.0))
 mat = material.Material("material0", "mymaterial", effect)
 mesh.effects.append(effect)
 mesh.materials.append(mat)
+'''
+path = os.path.join(dirname, "../textures/sand_texture.png")
+image = material.CImage("material_1-image", path)
+surface = material.Surface("material_1-image-surface", image)
+sampler2d = material.Sampler2D("material_1-image-sampler", surface)
+map1 = material.Map(sampler2d, "UVSET0")
+
+effect0 = material.Effect("material_0-effect", [], "lambert", emission=(0.0, 0.0, 0.0, 1),\
+                     ambient=(0.0, 0.0, 0.0, 1), diffuse=(0.890196, 0.882353, 0.870588, 1),\
+                     transparent=(1, 1, 1, 1), transparency=1.0, double_sided=True)
+effect1 = material.Effect("material_1-effect", [surface, sampler2d], "lambert", emission=(0.0, 0.0, 0.0, 1),\
+                     ambient=(0.0, 0.0, 0.0, 1),  diffuse=map1, transparent=map1, transparency=0.0, double_sided=True)
+
+mat0 = material.Material("material_0ID", "material_0", effect0)
+mat1 = material.Material("material_1ID", "material_1", effect1)
+
+mesh.effects.append(effect0)
+mesh.effects.append(effect1)
+
+mesh.materials.append(mat0)
+mesh.materials.append(mat1)
+
+mesh.images.append(image)
 
 # create source arrays
 '''
@@ -62,16 +88,23 @@ for x in range(cols):
         tmp = tmp / np.linalg.norm(tmp)
         normal_floats.append(tmp)
 
+xs /= (cols-1) * sc
+ys /= (rows-1) * sr
+uv_coords = np.stack((xs, ys), axis=2)
+uv_floats = uv_coords.flatten()
+
 vert_src = source.FloatSource("cubeverts-array", np.array(vert_floats), ('X', 'Y', 'Z'))
 normal_src = source.FloatSource("cubenormals-array", np.array(normal_floats), ('X', 'Y', 'Z'))
+uv_src = source.FloatSource("cubeuv-array", np.array(uv_floats), ('S', 'T'))
 
 # create geometry and add the sources
-geom = geometry.Geometry(mesh, "geometry0", "mycube", [vert_src, normal_src])
+geom = geometry.Geometry(mesh, "geometry", "mycube", [vert_src, normal_src, uv_src])
 
 # define inputs to triangle set
 input_list = source.InputList()
 input_list.addInput(0, 'VERTEX', "#cubeverts-array")
 input_list.addInput(1, 'NORMAL', "#cubenormals-array")
+input_list.addInput(2, 'TEXCOORD', "#cubeuv-array", set="0")
 
 # create index array
 '''
@@ -86,17 +119,20 @@ for x in range(cols-1):
         indices.append([id(x, y), id(x+1, y), id(x, y+1)])
         indices.append([id(x+1, y+1), id(x, y+1), id(x+1, y)])
 indices = np.array(indices).flatten()
-indices = np.stack((indices,indices), axis=1).flatten()
+indices = np.stack((indices,indices,indices), axis=1).flatten()
 
 # create triangle set, add it to list of geometries in the mesh
-triset = geom.createTriangleSet(indices, input_list, "materialref")
-geom.primitives.append(triset)
+triset0 = geom.createTriangleSet(indices, input_list, "material_0")
+triset1 = geom.createTriangleSet(indices, input_list, "material_1")
+geom.primitives.append(triset0)
+geom.primitives.append(triset1)
 mesh.geometries.append(geom)
 
 # instantiate geometry into a scene node
-matnode = scene.MaterialNode("materialref", mat, inputs=[])
-geomnode = scene.GeometryNode(geom, [matnode])
-node = scene.Node("node0", children=[geomnode])
+matnode0 = scene.MaterialNode("material_0", mat0, inputs=[])
+matnode1 = scene.MaterialNode("material_1", mat1, inputs=[])
+geomnode = scene.GeometryNode(geom, [matnode0, matnode1])
+node = scene.Node("Model", children=[geomnode])
 
 # create scene
 myscene = scene.Scene("myscene", [node])
