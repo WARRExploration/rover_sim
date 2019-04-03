ce5ca2c0ba2555b500e4085b55ae5cf0292b255d
Cymno <felix.bode@web.de>
2019-04-03 19:00:18 +0200

    unfinished master world generation script

--- scripts/generate_landmarks.py
+++ scripts/generate_landmarks.py
@@ -11,57 +11,62 @@ from shutil import copyfile
 
 import generate_single_landmark
 
-parser = ArgumentParser(description = "Adds landmarks to worlds")
-parser.add_argument("-l", "--landmarks", type=str, help = "Path to landmarks csv file", nargs="?",
-        default="../providedFiles/erc2018/Landmarks.csv")
-parser.add_argument("-o", "--output", type=str, help = "Path to .sdf file where the landmarks are inserted", nargs="?",
-        default="../worlds/erc2018_landmarks/model.sdf")
+# can be used as a standalone script
+# or be imported for use of the all_landmarks function
+if __name__ == '__main__':
+    parser = ArgumentParser(description = "Adds landmarks to worlds")
+    parser.add_argument("-l", "--landmarks", type=str, help = "Path to landmarks csv file", nargs="?",
+    #        default="../providedFiles/erc2018/Landmarks.csv")
+            default="../worlds/Generated/generationFiles/Landmarks.csv")
+    parser.add_argument("-o", "--output", type=str, help = "Path to .sdf file where the landmarks are inserted", nargs="?",
+            default="../worlds/erc2018_landmarks/model.sdf")
 
-args = parser.parse_args()
-
-# landmarks_path = "../../providedFiles/erc2018/Landmarks.csv"
-# output_path = "../../worlds/erc2018_landmarks/model.sdf"
+    args = parser.parse_args()
 
 
-with open (args.landmarks) as csvfile:
-    reader = csv.reader(csvfile)
-    next(reader, None)
+def all_landmarks_model(landmarks_path):
+    with open (landmarks_path) as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader, None)
+
+        landmarks = etree.Element('model')
+        landmarks.set('name', 'landmarks')
+        
+        for row in reader:
+            i = row[0][1:]
+
+            # (we define the pose in the <include>)
+            generate_single_landmark.create_all(int(i), '0 0 0 0 0 0')
+
+            model = etree.Element('model')
+            model.set('name', 'L' + i)
+
+            include = etree.Element('include')
+            
+            uri = etree.Element('uri')
+            uri.text = 'model://rover_sim/models/landmarks/L' + row[0][1:]
+
+            pose = etree.Element('pose')
+            pose.text = ' '.join(row[1:4]) + ' 0 0 0'
+
+            include.append(uri)
+            include.append(pose)
+            model.append(include)
+            
+            landmarks.append(model)
+
+    return landmarks
+
+
+if __name__ == '__main__':
+    landmarks = all_landmarks_model(args.landmarks)
 
     sdf = etree.Element('sdf')
     sdf.set('version', '1.6')
-
-    landmarks = etree.Element('model')
-    landmarks.set('name', 'landmarks')
-    
-    for row in reader:
-        i = row[0][1:]
-
-        # (we define the pose in the <include>)
-        generate_single_landmark.create_all(int(i), '0 0 0 0 0 0')
-
-        model = etree.Element('model')
-        model.set('name', 'L' + i)
-
-        include = etree.Element('include')
-        
-        uri = etree.Element('uri')
-        uri.text = 'model://rover_sim/models/landmarks/L' + row[0][1:]
-
-        pose = etree.Element('pose')
-        pose.text = ' '.join(row[1:4]) + ' 0 0 0'
-
-        include.append(uri)
-        include.append(pose)
-        model.append(include)
-        
-        landmarks.append(model)
-
-
     sdf.append(landmarks)
-
 
     tree = etree.ElementTree(sdf)
     print(args.output)
     tree.write(args.output, pretty_print=True, encoding='utf8', xml_declaration=True)
     #print(etree.tostring(args.output, pretty_print=True, encoding='utf8', xml_declaration=True))
-    
+        
---
+++ scripts/generate_world.py
@@ -0,0 +1,146 @@
+"""
+This script calls all the necessary generation scripts and creates a world folder
+from specified provided files. If the world already exists, it replaces 
+landmarks and terrain but keeps manual changes to the .world file
+"""
+from lxml import etree
+import os.path as op
+import os
+import copy
+from argparse import ArgumentParser
+from shutil import copy2, copyfile
+import subprocess
+
+import generate_landmarks
+
+
+parser = ArgumentParser(description = "Creates world in worlds directory")
+parser.add_argument("-w", "--world", type=str, help = "World name, updates world if already exists", nargs="?", default="Generated")
+parser.add_argument("-l", "--landmarks", type=str, help = "Path to landmarks csv file")
+parser.add_argument("-m", "--heightmap", type=str, help = "Path to heightmap csv file")
+parser.add_argument("-r", "--random", action="store_true", help = "Random heightmap and landmarks")
+
+args = parser.parse_args()
+print(args)
+
+
+
+dirname = op.dirname(__file__)
+base_path = op.join(dirname, op.pardir, "worlds", args.world)
+world_file = op.join(base_path, "world.world")
+gen_files = op.join(base_path, "generationFiles")
+
+landmarks_csv = op.join(gen_files, "Landmarks.csv")
+heightmap_csv = op.join(gen_files, "Heightmap.csv")
+
+# There is still no solution to the relative path issue:
+terran_path = "model://rover_sim/worlds/" + args.world + "/terrain.dae"
+
+
+
+if not op.isdir(gen_files):
+    os.makedirs(gen_files)
+
+if args.landmarks is not None:
+    copyfile(args.landmarks, landmarks_csv)
+
+if args.heightmap is not None:
+    copyfile(args.heightmap, heightmap_csv)
+    
+# .world file
+if op.exists(world_file):
+    try:
+        tree = etree.parse(world_file, etree.XMLParser(remove_blank_text=True))
+        root = tree.getroot()
+        world = root.find("world")
+
+    except:
+        backup_name = world_file + ".backup"
+        os.rename(world_file, backup_name)
+        print ("World structure corrupted, creating a new world. \nOld file was saved as: \n " + backup_name)
+        
+if not op.exists(world_file):    
+    root = etree.Element('sdf')
+    root.set("version", "1.3")
+    tree = etree.ElementTree (root)
+    world = etree.Element ('world')
+    world.set("name", "default")
+    root.append(world)
+
+
+# Terrain generation
+if args.random:
+    subprocess.call(["python", op.join(dirname, "generate_random_heightmap.py"),
+                "--output", heightmap_csv])
+
+# is the png image still necessary?
+subprocess.call(["python", op.join(dirname, "generate_heightmap.py"),
+                "--input", heightmap_csv,
+                "--output", op.join(base_path, "heightmap.png")])
+
+ 
+subprocess.call(["python", op.join(dirname, "generate_mesh.py"),
+                "--input", heightmap_csv,
+                "--output", op.join(base_path, "terrain.dae")])
+
+
+# World file
+
+def replace_elem(name, elem):
+    old_elem = world.find(name)
+    if old_elem is None:
+        world.append(elem)
+    else:
+        old_elem[:] = elem[:]
+
+scene = etree.Element("scene")
+grid = etree.SubElement(scene,"grid")
+grid.text = "false"
+replace_elem("scene", scene)
+
+include_sun = etree.Element("include")
+uri = etree.SubElement(include_sun,"uri")
+uri.text = "model://sun"
+replace_elem("include[1]",include_sun) # finds first <include>
+
+
+# Terrain inclusion
+# should probably be its own script
+terran = etree.Element("model")
+terran.set("name","terrain")
+static = etree.SubElement(terran,"static")
+static.text = "true"
+link = etree.SubElement(terran,"link")
+link.set("name","link")
+
+coll = etree.SubElement(link,"collision")
+coll.set("name","collision")
+c_mesh = etree.SubElement(etree.SubElement(coll,"geometry"),"mesh") # :3
+uri = etree.SubElement(c_mesh,"uri")
+uri.text = terran_path
+
+vis = etree.SubElement(link,"visual")
+vis.set("name","visual")
+v_mesh = etree.SubElement(etree.SubElement(vis,"geometry"),"mesh")
+uri = etree.SubElement(v_mesh,"uri")
+uri.text = terran_path
+
+pose = etree.SubElement(terran,"pose")
+pose.text = "0 0 0 0 0 0"
+replace_elem("model[@name='terrain']", terran)
+
+
+# Landmarks
+landmarks = generate_landmarks.all_landmarks_model(landmarks_csv)
+
+replace_elem("model[@name='landmarks']", landmarks)
+
+
+
+
+
+
+
+#print(etree.tostring(worl_file, pretty_print=True, encoding='utf8', xml_declaration=True))
+
+tree.write(world_file, pretty_print=True, encoding='utf8', xml_declaration=True)
